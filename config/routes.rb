6f1118ee2ca72e9bccbadda9d37ccfe4b8e93c82
Kenia Guimaraes <keniiarruda@gmail.com>
2017-12-13 23:13:59 +0000

    Ajuste na autenticacao

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => 'sistema', :path_names => {:sign_in => 'entra', :sign_out => 'sair', :sign_up => 'cadastrar'}
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount RailsAdmin::Engine => '/', as: 'rails_admin'
   
